9da72cffa31b6121a1412928cc9e68dedb451d6f
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-17 20:29:38 -0500

    feat(enrollments): updated progress record behavior.

--- src/main/java/com/acme/learning/platform/learning/domain/model/valueobjects/ProgressRecord.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/valueobjects/ProgressRecord.java
@@ -6,17 +6,63 @@ import com.acme.learning.platform.learning.domain.model.entities.Tutorial;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.OneToMany;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Embeddable
 public class ProgressRecord {
 
-    @OneToMany
+    @OneToMany(mappedBy = "enrollment")
     private List<ProgressRecordItem> progressRecordItems;
+
+    public ProgressRecord() {
+        progressRecordItems = new ArrayList<>();
+    }
 
     public void initializeProgressRecord(Enrollment enrollmentId, LearningPath learningPath) {
         Tutorial tutorialId = learningPath.getFirstTutorialInLearningPath();
         ProgressRecordItem progressRecordItem = new ProgressRecordItem(enrollmentId, tutorialId);
         progressRecordItems.add(progressRecordItem);
+    }
+
+    private ProgressRecordItem getProgressRecordItemWithTutorialId(Long tutorialId) {
+        return progressRecordItems.stream()
+                .filter(progressRecordItem -> progressRecordItem.getTutorial()
+                .getId().equals(tutorialId)).findFirst().orElse(null);
+    }
+
+    private boolean hasAnItemInProgress() {
+        return progressRecordItems.stream()
+                .anyMatch(ProgressRecordItem::isInProgress);
+    }
+
+    public void startTutorial(Long tutorialId) {
+        if (hasAnItemInProgress()) throw new IllegalStateException("A tutorial is already in progress");
+        ProgressRecordItem progressRecordItem = getProgressRecordItemWithTutorialId(tutorialId);
+        if (progressRecordItem != null) {
+            if (progressRecordItem.isNotStarted()) progressRecordItem.start();
+            else throw new IllegalStateException("Tutorial with given Id is already started or completed");
+        }
+        else throw new IllegalArgumentException("Tutorial with given Id not found in progress record");
+    }
+
+    public void completeTutorial(Long tutorialId, LearningPath learningPath) {
+        ProgressRecordItem progressRecordItem = getProgressRecordItemWithTutorialId(tutorialId);
+        if (progressRecordItem != null) progressRecordItem.complete();
+        else throw new IllegalArgumentException("Tutorial with given Id not found in progress record");
+
+        if (learningPath.isLastTutorialInLearningPath(tutorialId)) return;
+
+        Tutorial nextTutorial = learningPath.getNextTutorialInLearningPath(tutorialId);
+        if (nextTutorial != null) {
+            ProgressRecordItem nextProgressRecordItem = new ProgressRecordItem(progressRecordItem.getEnrollment(), nextTutorial);
+            progressRecordItems.add(nextProgressRecordItem);
+        }
+    }
+
+    public long calculateDaysElapsedForEnrollment(Enrollment enrollment) {
+        return progressRecordItems.stream().filter(progressRecordItem ->
+                        progressRecordItem.getEnrollment().equals(enrollment))
+                .mapToLong(ProgressRecordItem::calculateDaysElapsed).sum();
     }
 }
