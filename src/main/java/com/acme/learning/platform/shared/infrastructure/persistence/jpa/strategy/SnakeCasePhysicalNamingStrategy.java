2764e6a17da99a799980e78b733712d8c21564af
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-20 19:46:52 -0500

    chore: implemented snake case physical naming strategy for persistence objects naming.

---
+++ src/main/java/com/acme/learning/platform/shared/infrastructure/persistence/jpa/strategy/SnakeCasePhysicalNamingStrategy.java
@@ -0,0 +1,49 @@
+package com.acme.learning.platform.shared.infrastructure.persistence.jpa.strategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
+
+public class SnakeCasePhysicalNamingStrategy implements PhysicalNamingStrategy {
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(this.toPlural(identifier));
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return this.toSnakeCase(identifier);
+    }
+
+    private Identifier toSnakeCase(final Identifier identifier) {
+        if (identifier == null) {
+            return null;
+        }
+        final String regex = "([a-z])([A-Z])";
+        final String replacement = "$1_$2";
+        final String newName = identifier.getText().replaceAll(regex, replacement).toLowerCase();
+        return Identifier.toIdentifier(newName);
+    }
+
+    private Identifier toPlural(final Identifier identifier) {
+        final String newName = pluralize(identifier.getText());
+        return Identifier.toIdentifier(newName);
+    }
+}
