433726eba013beeaea86e31c598b63adc43d7ed5
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-24 21:04:07 -0500

    feat(learning): added servide annotation to student query service

--- src/main/java/com/acme/learning/platform/learning/application/internal/queryservices/StudentQueryServiceImpl.java
+++ src/main/java/com/acme/learning/platform/learning/application/internal/queryservices/StudentQueryServiceImpl.java
@@ -4,9 +4,11 @@ import com.acme.learning.platform.learning.domain.model.aggregates.Student;
 import com.acme.learning.platform.learning.domain.model.queries.GetStudentByProfileIdQuery;
 import com.acme.learning.platform.learning.domain.services.StudentQueryService;
 import com.acme.learning.platform.learning.infrastructure.persistence.jpa.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class StudentQueryServiceImpl implements StudentQueryService {
     private final StudentRepository studentRepository;
 
