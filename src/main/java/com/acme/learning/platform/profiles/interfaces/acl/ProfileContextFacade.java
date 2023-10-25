03651fe6b7a90026918b5042a3aaf7018778e697
Angel Velasquez <pcisavel@upc.edu.pe>
2023-10-24 20:40:36 -0500

    feat(profiles): implemented acl for profile aggregate in profiles context.

---
+++ src/main/java/com/acme/learning/platform/profiles/interfaces/acl/ProfileContextFacade.java
@@ -0,0 +1,34 @@
+package com.acme.learning.platform.profiles.interfaces.acl;
+
+
+import com.acme.learning.platform.profiles.domain.model.commands.CreateProfileCommand;
+import com.acme.learning.platform.profiles.domain.model.queries.GetProfileByEmailQuery;
+import com.acme.learning.platform.profiles.domain.model.valueobjects.EmailAddress;
+import com.acme.learning.platform.profiles.domain.services.ProfileCommandService;
+import com.acme.learning.platform.profiles.domain.services.ProfileQueryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileContextFacade {
+    private final ProfileCommandService profileCommandService;
+    private final ProfileQueryService profileQueryService;
+
+    public ProfileContextFacade(ProfileCommandService profileCommandService, ProfileQueryService profileQueryService) {
+        this.profileCommandService = profileCommandService;
+        this.profileQueryService = profileQueryService;
+    }
+
+    public Long createProfile(String firstName, String lastName, String email, String street, String number, String city, String zipCode, String country) {
+        var createProfileCommand = new CreateProfileCommand(firstName, lastName, email, street, number, city, zipCode, country);
+        return profileCommandService.handle(createProfileCommand);
+    }
+
+    public Long getProfileIdByEmail(String email) {
+        var getProfileByEmailQuery = new GetProfileByEmailQuery(new EmailAddress(email));
+        var profile = profileQueryService.handle(getProfileByEmailQuery);
+        if (profile.isEmpty()) {
+            return 0L;
+        }
+        return profile.get().getId();
+    }
+}
