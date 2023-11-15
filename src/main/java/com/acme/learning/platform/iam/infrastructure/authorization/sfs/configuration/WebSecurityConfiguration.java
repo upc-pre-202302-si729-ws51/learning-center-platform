6e6f40799f07c23594c16e0fe86041df59eca002
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-14 22:00:16 -0500

    feat(iam): completed implementation for security configuration.

--- src/main/java/com/acme/learning/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/acme/learning/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -9,10 +9,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * Web security configuration
+ */
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfiguration {
@@ -30,16 +37,30 @@ public class WebSecurityConfiguration {
         this.unauthorizedRequestHandler = unauthorizedRequestHandler;
     }
 
+    /**
+     * Authorization request filter
+     * @return authorization request filter
+     */
     @Bean
     public BearerAuthorizationRequestFilter authorizationRequestFilter() {
         return new BearerAuthorizationRequestFilter(tokenService, userDetailsService);
     }
 
+    /**
+     * Authentication manager
+     * @param authenticationConfiguration authentication configuration
+     * @return authentication manager
+     * @throws Exception if authentication manager cannot be created
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    /**
+     * Authentication provider
+     * @return authentication provider
+     */
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         var authenticationProvider = new DaoAuthenticationProvider();
@@ -48,10 +69,38 @@ public class WebSecurityConfiguration {
         return authenticationProvider;
     }
 
+    /**
+     * Password encoder
+     * @return password encoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return hashingService;
     }
 
-    // TODO: Add filterChain method
+    /**
+     * Filter chain
+     * @param http http security
+     * @return filter chain
+     * @throws Exception if filter chain cannot be created
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(unauthorizedRequestHandler))
+                .sessionManagement( customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/webjars/**").permitAll()
+                        .anyRequest().authenticated());
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authorizationRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
 }
