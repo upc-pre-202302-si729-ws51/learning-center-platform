8464433477d44db604f1adf3bfa4c4c6e9664a51
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-14 21:52:01 -0500

    feat(iam): added partial implementation for security configuration class.

---
+++ src/main/java/com/acme/learning/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.acme.learning.platform.iam.infrastructure.authorization.sfs.configuration;
+
+import com.acme.learning.platform.iam.infrastructure.authorization.sfs.pipeline.BearerAuthorizationRequestFilter;
+import com.acme.learning.platform.iam.infrastructure.hashing.bcrypt.BCryptHashingService;
+import com.acme.learning.platform.iam.infrastructure.tokens.jwt.BearerTokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfiguration {
+
+    private final UserDetailsService userDetailsService;
+    private final BearerTokenService tokenService;
+    private final BCryptHashingService hashingService;
+
+    private final AuthenticationEntryPoint unauthorizedRequestHandler;
+
+    public WebSecurityConfiguration(UserDetailsService userDetailsService, BearerTokenService tokenService, BCryptHashingService hashingService, AuthenticationEntryPoint unauthorizedRequestHandler) {
+        this.userDetailsService = userDetailsService;
+        this.tokenService = tokenService;
+        this.hashingService = hashingService;
+        this.unauthorizedRequestHandler = unauthorizedRequestHandler;
+    }
+
+    @Bean
+    public BearerAuthorizationRequestFilter authorizationRequestFilter() {
+        return new BearerAuthorizationRequestFilter(tokenService, userDetailsService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(hashingService);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return hashingService;
+    }
+
+    // TODO: Add filterChain method
+}
