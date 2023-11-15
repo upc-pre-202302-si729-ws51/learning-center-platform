dacae7cb99d16bcd7505bddbba44ca9f8102b984
Angel Velasquez <pcisavel@upc.edu.pe>
2023-11-14 20:03:57 -0500

    feat(roles): added query handler for get role by name query.

--- src/main/java/com/acme/learning/platform/iam/application/internal/queryservices/RoleQueryServiceImpl.java
+++ src/main/java/com/acme/learning/platform/iam/application/internal/queryservices/RoleQueryServiceImpl.java
@@ -2,11 +2,13 @@ package com.acme.learning.platform.iam.application.internal.queryservices;
 
 import com.acme.learning.platform.iam.domain.model.entities.Role;
 import com.acme.learning.platform.iam.domain.model.queries.GetAllRolesQuery;
+import com.acme.learning.platform.iam.domain.model.queries.GetRoleByNameQuery;
 import com.acme.learning.platform.iam.domain.services.RoleQueryService;
 import com.acme.learning.platform.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Role query service implementation
@@ -16,7 +18,6 @@ import java.util.List;
  * @see RoleQueryService
  * @see RoleRepository
  */
-
 @Service
 public class RoleQueryServiceImpl implements RoleQueryService {
     private final RoleRepository roleRepository;
@@ -26,10 +27,18 @@ public class RoleQueryServiceImpl implements RoleQueryService {
     }
 
     /**
-     * @InheritDoc
+     * Handle the get all roles query
      */
     @Override
     public List<Role> handle(GetAllRolesQuery query) {
         return roleRepository.findAll();
+    }
+
+    /**
+     * Handle the get role by name query
+     */
+    @Override
+    public Optional<Role> handle(GetRoleByNameQuery query) {
+        return roleRepository.findByName(query.name());
     }
 }
